1f93f661fed9d5c8db581969d6380c35733778d1
TGab99 <gwolf938@gmail.com>
2022-03-22 09:18:18 +0100

    Create arrays and constants

---
+++ js-numerals/js/functions.js
@@ -0,0 +1,7 @@
+let ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+let teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+
+const checkbox_number = document.getElementById("number");
+const checkbox_year = document.getElementById("year");
+const input_field = document.getElementById("input_field").value;
