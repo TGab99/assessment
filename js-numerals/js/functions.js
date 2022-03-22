15e247cf171413bf4e3197c01de8aa15ad1c4d95
TGab99 <gwolf938@gmail.com>
2022-03-22 09:47:44 +0100

    Write the convert_tens function and convert function, plus export convert_tens function

--- js-numerals/js/functions.js
+++ js-numerals/js/functions.js
@@ -2,6 +2,28 @@ let ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", 
 let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 let teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
 
-const checkbox_number = document.getElementById("number");
+/*const checkbox_number = document.getElementById("number");
 const checkbox_year = document.getElementById("year");
-const input_field = document.getElementById("input_field").value;
+const number = document.getElementById("input_field").value;
+const text = document.getElementById("text");*/
+
+function convert_tens(num) {
+    //num < 10 then return an element of ones array
+    if (num < 10) return ones[num];
+    //num >=10 and num < 20 return an element of teens array
+    else if (num >= 10 && num < 20) return teens[num - 10];
+    //num >= 10 and second character of num is equal 0 return an element of tens
+    else if (num >= 10 && num[1] == 0) return tens[num / 10];
+    //otherwise return an element of tens and an element of ones with "-"
+    else return tens[Math.floor(num / 10)] + "-" + ones[num % 10];
+}
+
+function convert(num) {
+    if (num == 0) return "zero";
+    else return convert_tens(num);
+}
+
+//exports for the tests too
+module.exports = {
+    convert_tens,
+};
