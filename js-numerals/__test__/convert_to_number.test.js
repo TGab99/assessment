7f033cd523fea3bcf11c090ba2f879f94e961674
TGab99 <gwolf938@gmail.com>
2022-03-22 10:18:07 +0100

    Test convert_hundreds function

--- js-numerals/__test__/convert_to_number.test.js
+++ js-numerals/__test__/convert_to_number.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@jest/globals");
 
-const {convert_tens} = require("../js/functions");
+const {convert_tens, convert_hundreds} = require("../js/functions");
 
 test("convert_tens passes for an number (7)", () => {
     expect(convert_tens(7)).toEqual("seven");
@@ -12,4 +12,24 @@ test("convert_tens passes for an number (47)", () => {
 
 test("convert_tens fails for a non-number", () => {
     expect(convert_tens("seven")).toEqual("seven");
+});
+
+test("convert_hundreds passes for an nunber (7)", () => {
+    expect(convert_hundreds(7)).toEqual("seven");
+});
+
+test("convert_hundreds passes for an nunber (47)", () => {
+    expect(convert_hundreds(47)).toEqual("forty-seven");
+});
+
+test("convert_hundreds passes for an nunber (100)", () => {
+    expect(convert_hundreds(100)).toEqual("one hundred");
+});
+
+test("convert_hundreds passes for an nunber (199)", () => {
+    expect(convert_hundreds(199)).toEqual("one hundred and ninety-nine");
+});
+
+test("convert_hundreds fails for an non-number", () => {
+    expect(convert_hundreds("one hundred")).toEqual("one hundred");
 });
